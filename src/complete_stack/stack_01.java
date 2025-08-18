11bf3c2965f5528f73e8d00aeebb6ac658e1d49b
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-18 09:46:16 +0530

    created the program to reverse the stack

--- src/complete_stack/stack_01.java
+++ src/complete_stack/stack_01.java
@@ -1,18 +1,24 @@
-//package complete_stack;
-//
-//// performing normal operations in the stack
-//import java.util.*;
-//public class stack_01 {
-//    public static void main(String[] args){
-//        Stack<Integer> st = new Stack<>();
-//
-//        st.push(1);
-//        st.push(2);
-//        st.push(4);
-//        st.push(5);
-//        System.out.println(st);
-//
-//        st.pop();
-//        System.out.println(st);
-//    }
-//}
+package complete_stack;
+
+// performing normal operations in the stack
+import java.util.*;
+public class stack_01 {
+    public static void main(String[] args){
+        Stack<Integer> st = new Stack<>();
+
+        st.push(1);
+        st.push(2);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+
+        st.pop();
+        System.out.println(st);
+
+        Stack<Integer> rt = new Stack<>();
+        while(st.size()>0){
+            rt.push(st.pop());
+        }
+        System.out.println(rt);
+    }
+}
